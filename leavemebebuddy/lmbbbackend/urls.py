6c476cba95071a84e962d9d31636e280ef14f131
lei-ezz <hanalei.ezz@gmail.com>
2024-06-10 21:04:52 +0100

    setup startup page

---
+++ leavemebebuddy/lmbbbackend/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    # two required: route and view, and two optional: kwargs, and name
+    path("", views.index, name="index"),
+]

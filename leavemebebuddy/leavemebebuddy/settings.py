501134ca2706af8ffe2f3e5ebd8f74c58ae6239d
lei-ezz <hanalei.ezz@gmail.com>
2024-06-10 23:32:02 +0100

    added model for users

--- leavemebebuddy/leavemebebuddy/settings.py
+++ leavemebebuddy/leavemebebuddy/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'lmbbbackend',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -74,6 +76,14 @@ WSGI_APPLICATION = 'leavemebebuddy.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    'random': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lmbb',
+        'USER': 'root',
+        'PASSWORD': 'BABAGHAN0UJ!',
+        'HOST': 'http://127.0.0.1/',
+        'PORT': '3306'
+    },
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
@@ -121,3 +131,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
--- leavemebebuddy/leavemebebuddy/urls.py
+++ leavemebebuddy/leavemebebuddy/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("lmbbbackend/", include("lmbbbackend.urls")),
+    path("lmbb/", include("lmbbbackend.urls")),
+    path("users/", include("users.urls")),
     path('admin/', admin.site.urls),
 ]
